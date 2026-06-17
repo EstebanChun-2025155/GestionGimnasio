73bd19c6b3bba1a2ff76c74d6c6e78de0f3f4e16
echun-2025155 <echun-2025155@kinal.edu.gt>
2026-06-17 11:31:14 -0600

    feat: Se cargo el modelo de la entidad ejercicio

---
+++ Gimnasio/src/models/ejercicio.ts
@@ -0,0 +1,10 @@
+export interface Ejercicio{
+    idEjercicio:number;
+    idRutina:number;
+    nombre:string;
+    grupoMuscular:string;
+    series:number;
+    repeticiones:number;
+    descanso:number;
+    instrucciones:string;
+}
