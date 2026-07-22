7b63d650b9ae1521cfb27301d05a236650f4e9d6
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-07-21 20:47:21 -0600

    refactor: Se modifico el nombre de los Types de Estados

--- Gimnasio/src/models/estado.ts
+++ Gimnasio/src/models/estado.ts
@@ -1,6 +1,5 @@
-export type Estado = "Activo"|"Inactivo";
+export type EstadoPersona = "Activo" | "Inactivo";
 
-export type estado = "activa"|"inactiva";
+export type EstadoHabilitacion = "activa" | "inactiva";
 
-export type Proceso = "En proceso"|"Finalizada";
-
+export type EstadoSeguimiento = "activa" | "finalizada";
