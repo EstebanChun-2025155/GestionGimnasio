610ec8b97a2dbdb73b968165ce204d2a842ebb63
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-07-25 10:06:59 -0600

    refactor: Se modificaron atributos del modelo Usuario

--- Gimnasio/src/models/usuario.ts
+++ Gimnasio/src/models/usuario.ts
@@ -2,11 +2,11 @@ import {EstadoPersona} from "./estado";
 import {Rol} from "./rol";
 
 export interface Usuario{
-    idUsuario:number,
+    id:number,
     username:string,
     password:string,
     rol:Rol,
     estado:EstadoPersona
-    fechaIngreso:Date
+    fechaIngreso:string
 }
 
